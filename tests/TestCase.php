ad2c85f112f71781924301ea150cb0a1a1a121fd
Hubert Krzysztofiak <hubertkrzyszt@gmail.com>
2024-08-28 08:34:12 +0200

    Add execute fresh command for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,21 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected static bool $setUpHasRunOnce = false;
+
+    /**
+     *
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        if (!static::$setUpHasRunOnce) {
+            Artisan::call('migrate:fresh');
+            static::$setUpHasRunOnce = true;
+        }
+    }
 }
