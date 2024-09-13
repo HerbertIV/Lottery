93cc5dcf07ba416b370383a6455d69c89b263b5c
Hubert Krzysztofiak <hubertkrzyszt@gmail.com>
2024-09-13 09:57:39 +0200

    Repair serverless cnfiguration

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', function (Request $request) {
-    return \App\Models\Product::all();
-})->middleware('auth:sanctum');
+Route::get('/test', function (Request $request) {
+    dd([
+        env('DB_CONNECTION'),
+        env('DB_HOST'),
+        env('DB_USERNAME'),
+        env('DB_PASSWORD'),
+    ]);
+});
