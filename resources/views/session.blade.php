52abd2220629d143d8da54d1d3680e233b8d456b
Hubert Krzysztofiak <hubertkrzyszt@gmail.com>
2024-09-24 08:11:31 +0200

    Remove css from public file

--- resources/views/session.blade.php
+++ resources/views/session.blade.php
@@ -1,16 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
-</head>
+@include('components.head')
 <body class="font-sans antialiased dark:bg-black dark:text-white/50">
 @if ($errors->any())
     <div class="alert alert-danger mt-4">
