de9b36663dfca9fc0d499deb93f3493c5073022d
Hubert Krzysztofiak <hubertkrzyszt@gmail.com>
2024-11-08 08:49:14 +0100

    Change title page

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Hoho losowanie</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
